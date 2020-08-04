ebf116dae67f316e7f303ba82c3cdd8261fc24c3
Willian17 <willianfernandes113@gmail.com>
2020-08-04 12:26:48 -0400

    aula-1

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -15,7 +15,7 @@ export default function Landing(){
             <div id="page-landing-content" className="container">
                 <div className="logo-container">
                     <img src={logoImg} alt="Proffy"/>
-                    <h2>Sua plataforma de estudos online.</h2>
+                    <h2>Sua plataforma de estudo online.</h2>
                 </div>
 
                 <img 
